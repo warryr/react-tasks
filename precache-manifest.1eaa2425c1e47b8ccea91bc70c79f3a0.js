d0cce99e518b79d0a0000b131965f22b759ca5b9
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-05-28 13:03:10 +0300

    Updates

---
+++ precache-manifest.1eaa2425c1e47b8ccea91bc70c79f3a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25300b3ed9b9165f2e711714c6539e5",
+    "url": "/react-tasks/index.html"
+  },
+  {
+    "revision": "8388ffbb20c7805d4f5e",
+    "url": "/react-tasks/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-tasks/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8388ffbb20c7805d4f5e",
+    "url": "/react-tasks/static/js/main.53ee2240.chunk.js"
+  },
+  {
+    "revision": "2c01daa980de8b699e68",
+    "url": "/react-tasks/static/js/runtime~main.1e41a676.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tasks/static/media/logo.5d5d9eef.svg"
+  }
+]);
