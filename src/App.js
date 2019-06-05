2b2a43e6f8ee589c7da6a402eb373dd2aac2f904
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-06-05 11:37:14 +0300

    feat: add browser history creation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './App.css';
 import ParentCounterContainer from './containers/ParentCounterContainer';
 import TabsContainer from './containers/TabsContainer';
 import AboutContainer from './containers/AboutContainer';
 import ErrorContainer from './containers/ErrorContainer';
 
+const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL,
+});
+
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Router>
+        <Router history={history}>
           <Switch>
             <Route path='/404' exact component={null}/>
             <Route path='/' component={TabsContainer}/>
