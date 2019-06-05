eba855d6702330ce2b06ece578a4d75096ac2363
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-06-05 11:04:32 +0300

    feat: add info to about page

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { useStyles } from './styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import aboutAgile from '../../img/about-agile.svg';
+import aboutExpertise from '../../img/about-expertise.svg';
+import aboutLearning from '../../img/about-learning.svg';
+import aboutPositive from '../../img/about-positive.svg';
+import aboutTechnologies from '../../img/about-technologies.svg';
+import Typography from "@material-ui/core/Typography";
 
 const About = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <p>Тут текст</p>
-    </div>
+    <Container fixed>
+      <Typography component='h1' variant='h4' className={classes.h4}>
+        Преимущества iTechArt
+      </Typography>
+      <Box className={classes.containerBox}>
+        <Box component='div' className={classes.contentBox}>
+          <img src={aboutAgile} alt='About agile' className={classes.image}/>
+          <Typography component='paragraph' variant='h6' className={classes.paragraph}>
+            Мы придерживаемся гибкого подхода как в менеджменте, так и в реализации новых идей
+          </Typography>
+        </Box>
+        <Box component='div' className={classes.contentBox}>
+          <img src={aboutExpertise} alt='About expertise' className={classes.image}/>
+          <Typography component='paragraph' variant='h6' className={classes.paragraph}>
+            У нас работают Remarkable People – высококлассные специалисты с глубокой технической экспертизой
+          </Typography>
+        </Box>
+        <Box component='div' className={classes.contentBox}>
+          <img src={aboutLearning} alt='About learning' className={classes.image}/>
+          <Typography component='paragraph' variant='h6' className={classes.paragraph}>
+            Мы, как и наши западные заказчики, предпочитаем новые технологии
+          </Typography>
+        </Box>
+        <Box component='div' className={classes.contentBox}>
+          <img src={aboutPositive} alt='About positive' className={classes.image}/>
+          <Typography component='paragraph' variant='h6' className={classes.paragraph}>
+            Мы любим учиться и делиться своими знаниями
+          </Typography>
+        </Box>
+        <Box component='div' className={classes.contentBox}>
+          <img src={aboutTechnologies} alt='About technologies' className={classes.image}/>
+          <Typography component='paragraph' variant='h6' className={classes.paragraph}>
+            У нас в компании доброжелательная и творческая атмосфера
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
---
+++ src/views/About/styles.js
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core';
+import grey from '@material-ui/core/colors/grey';
+
+export const useStyles = makeStyles(theme => ({
+  containerBox: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  contentBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '29%',
+    margin: theme.spacing(5, 2),
+    color: grey[700],
+  },
+  h4: {
+    margin: theme.spacing(3),
+  },
+  paragraph: {
+    margin: theme.spacing(2, 0),
+  },
+  image: {
+    width: '100px',
+    height: '100px',
+  }
+}));
