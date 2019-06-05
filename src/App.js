15c7a16cddcd26985a188f94992afff7cf045c33
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-06-05 11:52:08 +0300

    fix: fix routing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import './App.css';
 import ParentCounterContainer from './containers/ParentCounterContainer';
 import TabsContainer from './containers/TabsContainer';
 import AboutContainer from './containers/AboutContainer';
 import ErrorContainer from './containers/ErrorContainer';
 
-const history = createBrowserHistory({
-  basename: process.env.PUBLIC_URL,
-});
-
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Router history={history}>
+        <Router basename={process.env.PUBLIC_URL}>
           <Switch>
-            <Route path='/404' exact component={null}/>
-            <Route path='/' component={TabsContainer}/>
+            <Route path={process.env.PUBLIC_URL + '/404'} exact component={null}/>
+            <Route path={process.env.PUBLIC_URL + '/'} component={TabsContainer}/>
           </Switch>
           <Switch>
-            <Route path='/' exact component={null}/>
-            <Route path='/about' exact component={AboutContainer}/>
-            <Route path='/counters' exact component={ParentCounterContainer}/>
-            <Route path='/404' exact component={ErrorContainer}/>
-            <Redirect to='/404'/>
+            <Route path={process.env.PUBLIC_URL + '/'} exact component={null}/>
+            <Route path={process.env.PUBLIC_URL + '/about'} exact component={AboutContainer}/>
+            <Route path={process.env.PUBLIC_URL + '/counters'} exact component={ParentCounterContainer}/>
+            <Route path={process.env.PUBLIC_URL + '/404'} exact component={ErrorContainer}/>
+            <Redirect to={process.env.PUBLIC_URL + '/404'}/>
           </Switch>
         </Router>
       </div>
